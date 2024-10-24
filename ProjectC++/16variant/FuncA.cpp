9f60f803ae8d343e1d52e55603d63ea494667ea0
Trembach Anna <trembach.ann@gmail.com>
2024-10-24 19:16:08 +0300

    hotfix: implement sum of 3 terms

--- ProjectC++/16variant/FuncA.cpp
+++ ProjectC++/16variant/FuncA.cpp
@@ -1,5 +1,9 @@
 #include "funcA.h"
 
 double FuncA::calculate() {
-    return 0;  
+    double result = 0;
+    double x = 1; 
+    result += x - (pow(x, 3) / 6) + (3 * pow(x, 5) / 40);
+    return result;
 }
+
