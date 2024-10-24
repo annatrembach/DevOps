86ed0f1bca5ab5e8b084ee1cad4fa5f7fe87e1b1
Trembach Anna <trembach.ann@gmail.com>
2024-10-24 19:17:28 +0300

    branchA: implement full series calculation with n terms

--- ProjectC++/16variant/FuncA.cpp
+++ ProjectC++/16variant/FuncA.cpp
@@ -1,9 +1,18 @@
 #include "funcA.h"
 
+<<<<<<< Updated upstream
 double FuncA::calculate() {
     double result = 0;
     double x = 1; 
     result += x - (pow(x, 3) / 6) + (3 * pow(x, 5) / 40);
+=======
+double FuncA::calculate(int n) {
+    double result = 0;
+    double x = 1; 
+    for (int i = 0; i < n; ++i) {
+        result += (pow(-1, i) * (2 * i)!) / (pow(4, i) * pow(i!, 2) * (2 * i + 1)) * pow(x,2 * i + 1);
+    }
+>>>>>>> Stashed changes
     return result;
 }
 
